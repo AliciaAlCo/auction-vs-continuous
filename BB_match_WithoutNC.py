43265db8ce88d01eec60bf2bd4d6c532d05195a9
AliciaAlCo <79211798+AliciaAlCo@users.noreply.github.com>
2021-10-14 11:18:01 +0200

    Update BB_match_WithoutNC.py

--- BB_match_WithoutNC.py
+++ BB_match_WithoutNC.py
@@ -17,12 +17,6 @@ import sys
 
 #%% Testing inputs
 
-#Setpoint = pd.read_excel(open('Setpoint_nodes.xlsx', 'rb'),sheet_name='Nodes_15',index_col=0) # Baseline injections at each nodes (negative for retrieval)
-
-# # Initial Social Welfare and Flexibility Procurement Cost
-# Social_Welfare = 0
-# Flex_procurement = 0
-
 # Index for nodes
 bus = pd.read_excel(open('network33bus.xlsx','rb'),sheet_name='Bus',index_col=0)
 nodes = list(bus.index)
@@ -87,14 +81,13 @@ def BB_match_WithoutNC(setpoint,orderbook_temporary,offer_index,offer_quantity, 
      #%% Optimization problem 
         def req_selection():
             
-            print ('Calling optimization problem--------------------------------------------------')
+            print ('Calling optimization problem')
             
             m = pyo.ConcreteModel()
             
             # Sets creation
             m.N = pyo.Set(initialize = nodes)
             m.L = pyo.Set(initialize = lines)
-            
             m.RU = pyo.Set(initialize = requests_up.index)
             m.RD = pyo.Set(initialize = requests_down.index)
             
@@ -102,92 +95,45 @@ def BB_match_WithoutNC(setpoint,orderbook_temporary,offer_index,offer_quantity, 
             m.pru = pyo.Var(m.RU, domain=pyo.NonNegativeReals)
             m.prd = pyo.Var(m.RD, domain=pyo.NonNegativeReals)
             m.slack = pyo.Var(m.L, domain=pyo.NonNegativeReals)
-    
             m.theta = pyo.Var(m.N, domain=pyo.Reals) 
-            m.f = pyo.Var(m.L, domain=pyo.Reals)   #directed graph
-        
-        
-            # def flow_lim(model,L):
-            #     return m.f[L]+m.slack[L] >= -branch.loc[L,'Pmax']
-            # def flow_lim2(model,L):
-            #     return m.f[L]-m.slack[L] <= branch.loc[L,'Pmax']
-            
+            m.f = pyo.Var(m.L, domain=pyo.Reals)   
+
             def pru_lim(model,RU):
                 return m.pru[RU] <= requests_up.at[RU,'Quantity']
             def prd_lim(model,RD):
                 return m.prd[RD] <= requests_down.at[RD,'Quantity']
 
             # Constraints
-            m.ang_ref = pyo.Constraint(expr = m.theta[n_ref] == 0) # Constraint 1: Reference angle
-            m.flow_eq = pyo.ConstraintList() # Constraint 2: Distribution lines limits
-            # m.flow_bounds = pyo.Constraint(m.L, rule = flow_lim) # Constraint 2: Distribution lines limits
-            # m.flow_bounds2 = pyo.Constraint(m.L, rule = flow_lim2)
-            #m.nod_bal = pyo.ConstraintList() # Constraint 3: Power balance per node
-            #m.nod_bal.set_index('Node',inplace=True)
-            m.pru_limit = pyo.Constraint(m.RU, rule = pru_lim) # Constraint 5: Flexibility limits for requests and offers
-            m.prd_limit = pyo.Constraint(m.RD, rule = prd_lim) # Constraint 5: Flexibility limits for requests and offers
-            
+            m.ang_ref = pyo.Constraint(expr = m.theta[n_ref] == 0) # Reference angle
+            m.flow_eq = pyo.ConstraintList() # Distribution lines limits
+            m.pru_limit = pyo.Constraint(m.RU, rule = pru_lim) # Flexibility limits for requests and offers
+            m.prd_limit = pyo.Constraint(m.RD, rule = prd_lim) # Flexibility limits for requests and offers
             
             if direction == 'Up':
                 m.up = pyo.Constraint(expr = sum(m.pru[RU] for RU in m.RU) == pou)
             else:
                 m.down = pyo.Constraint(expr = sum(m.prd[RD] for RD in m.RD) == pod)
         
-            
             # objective function 
             m.obj_cost = pyo.Objective(expr= (pou + pod)*offer_price
                                        + sum(m.slack[L]*1000 for L in m.L) # One per line 
                                        - sum(m.pru[RU]*requests_up.at[RU,'Price'] for RU in m.RU) 
                                        - sum(m.prd[RD]*requests_down.at[RD,'Price'] for RD in m.RD), sense=pyo.minimize)
-    
-            # Constraint 2: Distribution lines limits
-            # for L in m.L:
-            #     m.flow_eq.add(m.f[L] - branch.loc[L,'B']*(m.theta[branch.loc[L,'From']] - m.theta[branch.loc[L,'To']]) == 0)          
                 
-            # Constraint 3: Power balance per node # Setpoint + Pou - Pod - Pru + Prd - losses = 0
-            # for N in m.N: 
-            #     if N == offer_bus:
-            #         m.nod_bal.add(Setpoint.at[0,N] + pou - pod
-            #                   - sum(m.pru[RU] for RU in m.RU if (requests_up.at[RU,'Bus'] == N)) 
-            #                   + sum(m.prd[RD] for RD in m.RD if (requests_down.at[RD,'Bus'] == N)) 
-            #                   # line flow
-            #                   - sum(branch.at[L,'B']*(m.theta[branch.at[L,'From']] - m.theta[branch.at[L,'To']]) for L in branch.index if branch.at[L,'From'] == N)
-            #                   - sum(branch.at[L,'B']*(m.theta[branch.at[L,'To']] - m.theta[branch.at[L,'From']]) for L in branch.index if branch.at[L,'To'] == N) == 0)
-            #     else:
-            #         m.nod_bal.add(Setpoint.at[0,N]
-            #                   - sum(m.pru[RU] for RU in m.RU if (requests_up.at[RU,'Bus'] == N)) 
-            #                   + sum(m.prd[RD] for RD in m.RD if (requests_down.at[RD,'Bus'] == N)) 
-            #                   # line flow
-            #                   - sum(branch.at[L,'B']*(m.theta[branch.at[L,'From']] - m.theta[branch.at[L,'To']]) for L in branch.index if branch.at[L,'From'] == N)
-            #                   - sum(branch.at[L,'B']*(m.theta[branch.at[L,'To']] - m.theta[branch.at[L,'From']]) for L in branch.index if branch.at[L,'To'] == N) == 0)
-               
-                
-            # Constraint 5: Flexibility limits for requests and offers
-            print ('Offer quantity')
-            print (offer_index,offer_quantity)
-            print ('Request quantity available')
+            # Constraint: Flexibility limits for requests and offers
             for ru in m.RU:
                 print (ru,requests_up.at[ru,'Quantity'])
-                #m.pru_limit.add(pyo.inequality(0,m.pru[ru],requests_up.at[ru,'Quantity']))
             for rd in m.RD:
                 print (rd,requests_down.at[rd,'Quantity'])
-                #m.prd_limit.add(pyo.inequality(0,m.prd[rd],requests_down.at[rd,'Quantity'])) 
 
             return m
-        
-        
         
         m = req_selection()
         
         #choose the solver
         opt = pyo.SolverFactory('glpk')
-        #opt.options["CPXchgprobtype"]="CPXPROB_FIXEDMILP"
-        #results = opt.solve(m,tee=True)
         results = opt.solve(m)
-        
-        #m.pprint()
-            
-        
+
         #Check if the problem if feasible
         print ('Check feasibility')
         slack = 0
@@ -197,7 +143,6 @@ def BB_match_WithoutNC(setpoint,orderbook_temporary,offer_index,offer_quantity, 
         
         if slack < epsilon:
             print ('Feasible')
-            #BB_matches = pd.DataFrame(columns = ['Offer','Offer Bus','Offer Block','Request','Request Bus','Direction','Quantity','Matching Price','Time_target','Social Welfare'])
 
             for RU in m.RU:
                 print (RU, m.pru[RU].value)
@@ -205,7 +150,6 @@ def BB_match_WithoutNC(setpoint,orderbook_temporary,offer_index,offer_quantity, 
                     continue
                 elif m.pru[RU].value > epsilon:
                     a = orderbook_temporary[orderbook_temporary['Request']==RU]
-                    print (a)
                     # Define matching price according to the arrival order
                     if a.iloc[-1,8] < a.iloc[-1,4]:
                         matching_price = a.iloc[-1,7]
@@ -234,23 +178,7 @@ def BB_match_WithoutNC(setpoint,orderbook_temporary,offer_index,offer_quantity, 
         else:
             print ('Infeasible')
         
-        
-        # checking that the optimization problem works properly
-        if (results.solver.status == SolverStatus.ok) and (results.solver.termination_condition == TerminationCondition.optimal):
-            print ("this is feasible and optimal")
-        elif results.solver.termination_condition == TerminationCondition.infeasible:
-            print ("do something about it? or exit?")
-        else:
-             # something else is wrong
-            print ('Something is wrong')
-            print (str(results.solver))
-            sys.exit('Error')
-            
-        
-
-    
         print (BB_matches) 
-        
+       
         return BB_matches
     
-#BB_match(Setpoint,orderbook_temporary,offer_index,offer_quantity, lines, nodes, direction, offer_bus, offer_price, offer_block)
